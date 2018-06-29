0b6ad08c1a18bffe96943a3c7c3578179a3ee236
balbano80 <balbano80@gmail.com>
2018-06-29 11:15:24 -0700

    admin, company, crew, employee models done.  Others are in progress and relationships on a few need to be updated

---
+++ models/employee.js
@@ -0,0 +1,53 @@
+module.exports = function (sequelize, DataTypes) {
+    var Employee = sequelize.define("Employee", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        last_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull:false,
+            isEmail: true
+        },
+        phone_number: { 
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                len: [9]
+            }
+        }
+    });
+
+
+    Employee.associate = function (models) {
+        // We're saying that a Asset should belong to an User
+        // An Asset can't be created without an user due to the foreign key constraint
+        Employee.belongsTo(models.Crew, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+
+        Employee.hasMany(models.Employee-cert, {
+            onDelete: "cascade"
+        });
+
+    };
+
+    return Employee;
+}
