7bbac5ac30eb8324ac35c290b3998c271bde31a2
basosa94 <basosa@ucdavis.edu>
2018-06-29 09:31:30 -0700

    modify API routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,6 +6,10 @@ module.exports = function (app) {
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
+  app.get("/", function(req,res){
+    res.send("Working");
+  })
+
   app.post("/api/login", passport.authenticate("local"), function (req, res) {
     // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
     // So we're sending the user back the route to the members page because the redirect will happen on the front end
