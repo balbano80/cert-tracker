bb8ab2e390f10f9d59b7c79798708e8f4ad239bb
basosa94 <basosa@ucdavis.edu>
2018-07-05 12:47:38 -0700

    Add functionality to edit cert modal

--- client/components/DashbAddCertificationModal.js
+++ client/components/DashbAddCertificationModal.js
@@ -3,6 +3,7 @@ import { Container, Row, Col, Input, Button, Table, Fa, Card, CardBody, Modal, M
 import DashbDropdownReminder from '../components/DashbDropdownReminder';
 import DashbDropdownValidfor from '../components/DashbDropdownValidfor';
 
+
 const modStyle = {
   'text': 'center',
   'background-color': '#b1bace',
@@ -13,12 +14,40 @@ class DashbAddCertificationModal extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      modal: false
-
+      modal: false,  
+      certification: "",
+      reminder: "",
+      validFor:"",
+      supervisorEmail:"",
+      supervisorPhone:""
     }
     this.toggle = this.toggle.bind(this);
 
   }
+
+  handleInputChange = event => {
+    // Getting the value and name of the input which triggered the change
+    const { name, value } = event.target;
+
+    // Updating the input's state
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    event.preventDefault();
+    if (!this.state.certification || !this.state.supervisorEmail || !this.state.reminder || !this.state.supervisorEmail || !this.state.validFor) {
+      alert("Fill out all fields");
+    } else {
+      console.log("Reminder:" + this.state.reminder )
+      this.props.populate(<div>{this.state.certification}</div>)
+  
+    }
+
+
+  };
 
   toggle() {
     this.setState({
@@ -36,7 +65,6 @@ class DashbAddCertificationModal extends React.Component {
             <Modal isOpen={this.state.modal} toggle={this.toggle} size="fluid">
               <ModalHeader className="blue-grey-text" toggle={this.toggle}>Add Certification</ModalHeader>
               <ModalBody className="blue-grey-text">
-
                 <Row>
                   <Col>
                     <Table>
@@ -51,11 +79,11 @@ class DashbAddCertificationModal extends React.Component {
                       </thead>
                       <tbody>
                         <tr>
-                          <td><Input size="sm" label="Certification" icon="list-ul" group type="text" validate error="wrong" success="right" /></td>
-                          <td><DashbDropdownValidfor /></td>
-                          <td><DashbDropdownReminder /></td>
-                          <td><Input size="sm" label="Supervisor Email" icon="envelope" group type="text" validate error="wrong" success="right" /></td>
-                          <td><Input size="sm" label="Supervisor Phone" icon="phone" group type="text" validate error="wrong" success="right" /></td>
+                          <td><Input name ="certification" value={this.state.certification} onChange={this.handleInputChange} size="sm" label="Certification" icon="list-ul" group type="text" validate error="wrong" success="right" /></td>
+                          <td><DashbDropdownValidfor/></td>
+                          <td><DashbDropdownReminder/></td>
+                          <td><Input name = "supervisorEmail" value={this.state.supervisorEmail} onChange={this.handleInputChange} size="sm" label="Supervisor Email" icon="envelope" group type="text" validate error="wrong" success="right" /></td>
+                          <td><Input name = "supervisorPhone" value={this.state.supervisorPhone} onChange={this.handleInputChange} size="sm" label="Supervisor Phone" icon="phone" group type="text" validate error="wrong" success="right" /></td>
                         </tr>
                       </tbody>
                     </Table>
@@ -64,7 +92,7 @@ class DashbAddCertificationModal extends React.Component {
               </ModalBody>
               <ModalFooter>
                 <Button color="secondary" onClick={this.toggle}>Close</Button>{' '}
-                <Button color="primary">Save changes</Button>
+                <Button color="primary" onClick={this.handleFormSubmit}>Save changes</Button>
               </ModalFooter>
             </Modal>
           </Col>
@@ -73,5 +101,67 @@ class DashbAddCertificationModal extends React.Component {
     );
   }
 };
+
+
+// const modal ={
+//   constructor(props) {
+//       super(props);
+//       props.state = {
+//         modal: false,  
+//       }
+//     props.toggle = props.toggle.bind(props);
+
+//     toggle() {
+//       props.setState({
+//         modal: !props.state.modal
+
+//       });
+//     }
+//   }
+// };
+
+// const DashbAddCertificationModal = props => (
+
+// <Container>
+//     <Row>
+//       <Col size="12" className="text-center mb-5">
+//         <Button outline color="primary" onClick={props.toggle}>Add Certification</Button>
+//         <Modal isOpen={props.state.modal} toggle={props.toggle} size="fluid">
+//           <ModalHeader className="blue-grey-text" toggle={props.toggle}>Add Certification</ModalHeader>
+//           <ModalBody className="blue-grey-text">
+//             <Row>
+//               <Col>
+//                 <Table>
+//                   <thead>
+//                     <tr>
+//                       <th className="text-center">Certification</th>
+//                       <th className="text-center">Valid For</th>
+//                       <th className="text-center">Reminders At</th>
+//                       <th className="text-center">Supervisor Email</th>
+//                       <th className="text-center">Supervisor Phone</th>
+//                     </tr>
+//                   </thead>
+//                   <tbody>
+//                     <tr>
+//                       <td><Input name ="certification" value={props.state.certification} onChange={props.handleInputChange} size="sm" label="Certification" icon="list-ul" group type="text" validate error="wrong" success="right" /></td>
+//                       <td><DashbDropdownValidfor /></td>
+//                       <td><DashbDropdownReminder /></td>
+//                       <td><Input name = "supervisorEmail" value={props.state.supervisorEmail} onChange={props.handleInputChange} size="sm" label="Supervisor Email" icon="envelope" group type="text" validate error="wrong" success="right" /></td>
+//                       <td><Input name = "supervisorPhone" value={props.state.supervisorPhone} onChange={props.handleInputChange} size="sm" label="Supervisor Phone" icon="phone" group type="text" validate error="wrong" success="right" /></td>
+//                     </tr>
+//                   </tbody>
+//                 </Table>
+//               </Col>
+//             </Row>
+//           </ModalBody>
+//           <ModalFooter>
+//             <Button color="secondary" onClick={props.toggle}>Close</Button>{' '}
+//             <Button color="primary" onClick={props.handleFormSubmit}>Save changes</Button>
+//           </ModalFooter>
+//         </Modal>
+//       </Col>
+//     </Row>
+//   </Container>
+// );
 
 export default DashbAddCertificationModal;
--- client/components/DashbMainCertModal.js
+++ client/components/DashbMainCertModal.js
@@ -1,38 +1,50 @@
 import React from 'react';
-import { Card, CardBody, Table,  } from 'mdbreact';
+import { Card, CardBody, Table, } from 'mdbreact';
 import DashbAddCertificationModal from '../components/DashbAddCertificationModal';
 import DashbEditCertificationModal from '../components/DashbEditCertificationModal';
+import CertificationTable from '../components/CertificationTable';
 
-class DashbMainCertModal extends React.Component  {
-  
+class DashbMainCertModal extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            certNameArray:[],
+            reminderArray:[],
+            validForArray:[]
+        }
+    }
 
-  render() {
-    return(
-        <div>
-            <Card>
-                <CardBody>
-                <h4 className="h4-responsive text-center">Certifications</h4>
-                <DashbEditCertificationModal />
-                <DashbAddCertificationModal />
-                <Table striped bordered small>
-                    <tbody>
-                    <tr>
-                        <td>Fire Safety</td>
-                    </tr>
-                    <tr>
-                        <td>Sexual Harrassment Training</td>
-                    </tr>
-                    <tr>
-                        <td>Ladder Safety</td>
-                    </tr>
-                    </tbody>
-                </Table>
-                </CardBody>
-            </Card>
-        </div>
+    populate = (cert) => {
+        let newArray= this.state.certNameArray;
+        // let newRemindArray=this.state.reminderArray;
+        // let newValidForArray=this.state.validForArray;
+        newArray.push(cert)
+        this.setState({
+            certNameArray: newArray,
+            // reminderArray: newRemindArray,
+            // validForArray: newValidForArray
+        })
+    };
 
-    );
-  }
+    
+    render() {
+        return (
+            <div>
+                <Card>
+                    <CardBody>
+                        <h4 className="h4-responsive text-center">Certifications</h4>
+                        <DashbEditCertificationModal />
+                        <DashbAddCertificationModal populate={this.populate} />
+                        <Table striped bordered small>
+                            
+                            <CertificationTable cert={this.state.certNameArray}/>
+                            
+                        </Table>
+                    </CardBody>
+                </Card>
+            </div>
+        );
+    }
 };
 
 export default DashbMainCertModal;
