4e9e172c08e113b83846f2b4abcc5ddf3d5f2ad5
basosa94 <basosa@ucdavis.edu>
2018-07-06 13:22:42 -0700

    add js files to render site names on side nav

---
+++ controllers/siteController.js
@@ -0,0 +1,51 @@
+const db = require("../models");
+
+// Defining methods for the SiteController
+
+
+module.exports = {
+  findAll: function(req, res) {
+    db.Site.findAll({})
+    .then(function(dbSite) {
+      res.json(dbSite);
+      res.json('{message: "hello"}')
+    });
+  },
+  findById: function(req, res) {
+    db.Site.findOne({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(function(dbSite) {
+        res.json(dbSite);
+      });
+  },
+  create: function(req, res) {
+    db.Site.create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+     db.Site.update(req.body,
+      {
+        where: {
+          id: req.body.id
+        }
+      })
+      .then(function(dbSite) {
+        res.json(dbSite);
+      });
+
+  },
+  remove: function(req, res) {
+    db.Site.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(function(dbSite) {
+        res.json(dbSite);
+      });
+  }
+};
