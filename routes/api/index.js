13d0efb45e66b194c5015998bc2f2287da82f7a5
climbah <mike@joomla-go.com>
2018-07-08 14:37:32 -0700

    fixed route file

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,22 +1,18 @@
 const router = require("express").Router();
 const employeeRoutes = require("./employees");
-const adminRoutes = require("./admin");
 const siteRoutes = require("./site");
 const employeeCertRoutes = require("./employeeCerts");
 const certificatesRoutes = require("./certificates");
 const userRoutes = require("./user");
-// const loginRoutes = require("./login");
 const companyRoutes = require("./company");
 
 
 // Employee routes
 router.use("/employee", employeeRoutes);
-router.use("/admin", adminRoutes);
 router.use("/site", siteRoutes);
 router.use("/employeecerts", employeeCertRoutes);
 router.use("/certificates", certificatesRoutes);
 router.use("/user", userRoutes);
-// router.use("/login", loginRoutes);
 router.use("/company", companyRoutes);
 
 
